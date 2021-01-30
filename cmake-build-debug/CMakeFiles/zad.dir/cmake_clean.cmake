e57aa0c61f974a2e9522cdbe47e0198f95657741
Furmanczyk Dominik <fud@emea.baumernet.org>
2021-01-30 18:44:54 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/zad.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zad.dir/main.c.obj"
+  "libzad.dll.a"
+  "zad.exe"
+  "zad.exe.manifest"
+  "zad.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
